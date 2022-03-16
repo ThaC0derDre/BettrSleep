fdceb14f263b5b03454efd6ffdd99f593df03699
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-15 17:02:14 -0700

    Initial Commit

---
+++ BettrSleep/BettrSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BettrSleepApp.swift
+//  BettrSleep
+//
+//  Created by Andres Gutierrez on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BettrSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
