1ef38bdd0c5015f77cce4222e6107148c5c64819
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-17 13:13:44 -0700

    complete challenges for ui optimization

--- BettrSleep/ContentView.swift
+++ BettrSleep/ContentView.swift
@@ -9,58 +9,67 @@ import SwiftUI
 import CoreML
 
 struct ContentView: View {
-    @State private var wakeUp   = defaultTime
+    @State private var wakeUp       = defaultTime
     @State private var sleepAmount  = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle   = ""
-    @State private var alertMessage = ""
-    @State private var showAlert    = false
+    @State private var recSleepTime = ""
     static var defaultTime: Date {
         var component = DateComponents()
         component.hour      = 7
         component.minute    = 0
         return Calendar.current.date(from: component) ?? Date.now
     }
+    
+    
     var body: some View {
         NavigationView {
             Form{
-                VStack (alignment: .leading, spacing: 0) {
+                Section {
                     Text("When do you want to wake up?")
-                        .font(.headline)
-                    
+                            .font(.headline)
                     
                     HStack{
                         Spacer()
                     DatePicker("Select time to wake up", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                         .padding(.trailing)
+                        
+                        
                 }
                 }
-                VStack (alignment: .leading, spacing: 0) {
+                .listRowSeparator(.hidden)
+                Section {
                     Text("How many hours of sleep would you like?")
                         .font(.headline)
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25, onEditingChanged: { _ in
+                        calculateSleep()
+                    })
                         .padding(.horizontal)
-                    
                 }
-                VStack (alignment: .leading, spacing: 0) {
+                .listRowSeparator(.hidden)
+                Section {
                     Text("Daily coffee intake")
                         .font(.headline)
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                   
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20, onEditingChanged: { _ in
+                        calculateSleep()
+                    })
                         .padding(.horizontal)
+                }
+                .listRowSeparator(.hidden)
+                
+                VStack(alignment: .center) {
+                    
+                    Text("You should sleep at \(recSleepTime) then!")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        
                 }
             }
             .navigationTitle("BettrRest")
-            .toolbar {
-                Button("Calculate", action: calculateSleep)
-                
-                    .alert(alertTitle, isPresented: $showAlert) {
-                        Button("OK") {}
-                    } message: {
-                        Text(alertMessage)
-                    }
-            }
+            .onAppear(perform: calculateSleep)
         }
         
     }
@@ -69,27 +78,25 @@ struct ContentView: View {
         do{
             
             
-            let config  = MLModelConfiguration()
+            let config      = MLModelConfiguration()
             
-            let model   = try SleepCalculator(configuration: config)
+            let model       = try SleepCalculator(configuration: config)
             
             let components  = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute  = (components.minute ?? 0) * 60
+            let hour        = (components.hour ?? 0) * 60 * 60
+            let minute      = (components.minute ?? 0) * 60
             
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let prediction  = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
-            let sleepTime = wakeUp - prediction.actualSleep
+            let sleepTime   = wakeUp - prediction.actualSleep
             
-            alertTitle      = "Your suggested sleep time.."
-            alertMessage    = sleepTime.formatted(date: .omitted, time: .shortened)
+            recSleepTime    = sleepTime.formatted(date: .omitted, time: .shortened)
             
             
         }catch {
-            alertTitle      = "Oh no.."
-            alertMessage    = "There was an error calculating your sleep time. Please try again."
+            
         }
-        showAlert = true
+     
     }
 }
 
